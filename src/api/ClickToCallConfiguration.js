35a97eba024b5a78a803d5b9eb75eeca142a9faf
ilGuro <ronaldgutierrez@unicauca.edu.co>
2022-10-16 12:47:59 -0500

    Phone number changed

--- src/api/ClickToCallConfiguration.js
+++ src/api/ClickToCallConfiguration.js
@@ -1,6 +1,6 @@
 export const ClickToCallConfiguration = {
     domain: 'rtc.engagedigital.ai',
-    callToNum: '900848',//'9070707120',
+    callToNum: '900854',//'9070707120',
     callType: 'video',
     consoleLog: true,
     joinWithVideoMuted: false,
